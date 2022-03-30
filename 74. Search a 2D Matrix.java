c41138f7e85c98d4552d0b0270c34423f6086b20
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2022-03-30 13:49:10 +0530

    Update 74. Search a 2D Matrix.java

--- 74. Search a 2D Matrix.java
+++ 74. Search a 2D Matrix.java
@@ -26,3 +26,29 @@ class Solution {
         return false;
     }
 }
+
+
+ // OPTIMISE APPROCH
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=0;
+        int col=matrix[0].length-1;
+        
+        while(row<matrix.length && col>=0)
+        {
+            if(target==matrix[row][col]) return true;
+            
+            else if(target<matrix[row][col])
+            {
+                col--;
+            }
+            else 
+            {
+                row++;
+            }
+        }
+        
+        return false;
+    }
+}
